20bef499c89a4b3da7ddb9c550ac79a5e926fa0d
damr <damr18@gmail.com>
2021-02-16 22:56:19 -0600

    fetch hardcoded author

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -10,22 +10,53 @@ class Articles extends React.Component {
     };
   }
 
+  fetchArticles = () => {
+    fetch(`https://jsonmock.hackerrank.com/api/articles?author=saintamh&page=1`)
+      .then((res) => res.json())
+      .then((json) => {
+        let arr = [];
+        json.data.map((el) => {
+          if (el.title != null) {
+            arr.push(el);
+          }
+        });
+
+        this.setState({ articlesList: arr, firstRequest: true });
+      });
+  };
   render() {
     return (
-      <div className="controls">
-        <div className="input-container">
-          <span>author:</span>
-          <input
-            type="text"
-            className="text-input"
-            data-testid="text-input"
-            name="authorName"
-          />
-          <button className="fetch-button" data-testid="fetch-button">
-            Fetch
-          </button>
+      <React.Fragment>
+        <div className="controls">
+          <div className="input-container">
+            <span>author:</span>
+            <input
+              type="text"
+              className="text-input"
+              data-testid="text-input"
+              name="authorName"
+            />
+            <button
+              className="fetch-button"
+              data-testid="fetch-button"
+              onClick={this.fetchArticles}
+            >
+              Fetch
+            </button>
+          </div>
         </div>
-      </div>
+        {this.state.articlesList.length > 0
+          ? this.state.articlesList.map((el, i) => {
+              return (
+                <div key={i} className="results">
+                  <li key="example-key" data-testid="result-row">
+                    {el.title}
+                  </li>
+                </div>
+              );
+            })
+          : null}
+      </React.Fragment>
     );
   }
 }
