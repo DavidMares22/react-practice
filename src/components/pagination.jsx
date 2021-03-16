22b6b33ee160206516b8e0343331cdc8d08e7cce
damr <damr18@gmail.com>
2021-03-16 10:54:54 -0600

    propTypes for pagination

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,21 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 import _ from "lodash";
 
 const Pagination = (props) => {
-  const { itemsCount, pageSize } = props;
+  const { itemsCount, pageSize, currentPage, onPageChange } = props;
 
-  const pagesCount = itemsCount / pageSize;
+  const pagesCount = Math.ceil(itemsCount / pageSize);
 
+  if (pagesCount === 1) return null;
   const pages = _.range(1, pagesCount + 1);
-
-  
 
   return (
     <nav>
       <ul className="pagination">
         {pages.map((page) => (
-          <li key={page} className={page === props.currentPage? 'page-item active': 'page-item'}>
-            <button className="page-link" onClick={() => props.onPageChange(page)}>
+          <li
+            key={page}
+            className={page === currentPage ? "page-item active" : "page-item"}
+          >
+            <button className="page-link" onClick={() => onPageChange(page)}>
               {page}
             </button>
           </li>
@@ -23,6 +26,13 @@ const Pagination = (props) => {
       </ul>
     </nav>
   );
+};
+
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange:PropTypes.func.isRequired
 };
 
 export default Pagination;
