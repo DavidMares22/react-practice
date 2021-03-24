4c5b62ebe00d83300f1e51ec4db504bc5d1cadc6
damr <damr18@gmail.com>
2021-03-24 08:48:48 -0600

    loginForm UI

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navbar from "./components/navbar";
 import MovieForm from './components/movieForm';
 import NotFound from './components/notFound';
 import Rentals from './components/rentals';
+import LoginForm from './components/loginForm';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/movies" component={Movies} />
           <Route path="/customers" component={Customers} />
           <Route path="/rentals" component={Rentals} />
+          <Route path="/login" component={LoginForm} />
           <Route path="/not-found" component={NotFound} />
           <Redirect from="/" exact to="/movies" />
           <Redirect  to="/not-found" />
