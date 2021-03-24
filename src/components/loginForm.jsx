f880204dc3ca6cae4544709b73a665cf1d12b422
damr <damr18@gmail.com>
2021-03-24 09:23:32 -0600

    change state according to inputs

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,21 +1,21 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
-  //   username = React.createRef();
-
-  //   componentDidMount() {
-  //     this.username.current.focus();
-  //   }
+  state = {
+    account: { username: "", password: "" },
+  };
 
   handleSubmit = (e) => {
     e.preventDefault();
-
-    // const username = this.username.current.value;
-
-    // console.log(username);
+  };
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
   };
 
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -24,15 +24,24 @@ class LoginForm extends Component {
             <label htmlFor="username">Username</label>
             <input
               autoFocus
+              value={account.username}
+              onChange={this.handleChange}
               id="username"
+              name="username"
               type="text"
               className="form-control"
-              //   ref={this.username}
             />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              id="password"
+              name="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
