397b957d47a565343de91c942f8ec84e9ad01d69
damr <damr18@gmail.com>
2021-03-25 11:28:43 -0600

    autoFocuse refactor

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,29 +1,21 @@
 import React from "react";
 
 const Input = ({ name, label, value, onChange, error, focus }) => {
+ 
+
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      {focus ? (
-        <input
-          autoFocus
-          value={value}
-          onChange={onChange}
-          id={name}
-          name={name}
-          type="text"
-          className="form-control"
-        />
-      ) : (
-        <input
-          value={value}
-          onChange={onChange}
-          id={name}
-          name={name}
-          type="text"
-          className="form-control"
-        />
-      )}
+
+      <input
+        autoFocus = {focus || false}
+        value={value}
+        onChange={onChange}
+        id={name}
+        name={name}
+        type="text"
+        className="form-control"
+      />
 
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
