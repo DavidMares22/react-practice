13f64da1422c46374546f1cfc1c60f6cdfad6002
damr <damr18@gmail.com>
2021-03-28 12:02:13 -0600

    get posts and display in table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    posts: [],
+  };
+
+  async componentDidMount() {
+    const { data: posts } = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    this.setState({ posts });
+  }
+
+  render() {
+    return (
+      <main className="container">
+        <button className="btn btn-primary">Add</button>
+        {this.state.posts && (
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Update</th>
+                <th scope="col">Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.posts.map((p) => {
+                return (
+                  <tr key={p.id}>
+                    <th scope="row">{p.id}</th>
+                    <td>{p.title}</td>
+                    <td>
+                      <button className="btn btn-info">Update</button>
+                    </td>
+                    <td>
+                      <button className="btn btn-danger">Delete</button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+      </main>
+    );
+  }
 }
 
 export default App;
