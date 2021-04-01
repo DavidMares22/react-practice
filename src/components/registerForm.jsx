b20b0926b3e84e6eac639732373bb423a5d067ac
damr <damr18@gmail.com>
2021-04-01 10:26:51 -0600

    display error in case of duplicate email

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,34 +1,44 @@
-import React  from "react";
+import React from "react";
 
 import Joi from "joi-browser";
 import Form from "./form";
 
+import * as userService from "../services/userService";
+
 class RegisterForm extends Form {
   state = {
-    data: { username: "", password: "", name:"" },
+    data: { username: "", password: "", name: "" },
     errors: {},
   };
 
   schema = {
-    username: Joi.string().required().label("Username").trim(),
+    username: Joi.string().required().email().trim(),
     password: Joi.string().required().label("Password").trim().min(5),
     name: Joi.string().required().label("Name").trim(),
   };
 
-  doSubmit = () => {
+  doSubmit = async () => {
     // call the server
+    try {
+      await userService.register(this.state.data);
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = error.response.data;
+        this.setState({ errors });
+      }
+    }
     console.log("submitted");
   };
 
   render() {
-    
     return (
       <div>
         <h1>Register</h1>
         <form onSubmit={this.handleSubmit}>
           {this.renderInput("username", "Username", true)}
-          {this.renderInput("password", "Password",false, "password")}
-          {this.renderInput("name", "Name",false)}
+          {this.renderInput("password", "Password", false, "password")}
+          {this.renderInput("name", "Name", false)}
 
           {this.renderButton("Register")}
         </form>
