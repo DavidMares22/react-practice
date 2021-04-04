4d31ffc1032280829396e0f3f6e140accad8e111
damr <damr18@gmail.com>
2021-04-04 10:55:59 -0500

    send auth token in header

--- src/services/authService.js
+++ src/services/authService.js
@@ -5,24 +5,32 @@ import jwtDecode from "jwt-decode";
 
 const apiEndpoint = config.apiUrl + "/auth";
 
+const tokenKey = "token"
+
+http.setJwt(getJwt())
+
+
 export async function login(email, password) {
   const { data: jwt } = await http.post(apiEndpoint, { email, password });
-  localStorage.setItem("token", jwt);
+  localStorage.setItem(tokenKey, jwt);
 }
 
 export function loginWithJwt(jwt) {
-  localStorage.setItem("token", jwt);
+  localStorage.setItem(tokenKey, jwt);
 }
 
 export function logout() {
-  localStorage.removeItem("token");
+  localStorage.removeItem(tokenKey);
 }
 
 export function getCurrentUser() {
   try {
-    const jwt = localStorage.getItem("token");
+    const jwt = localStorage.getItem(tokenKey);
     return jwtDecode(jwt);
   } catch (error) {
     return null;
   }
+}
+export function getJwt() {
+  return localStorage.getItem(tokenKey)
 }
