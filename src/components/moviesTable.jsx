3fb68f72e6205ece3191b1df2dee01fa4d814d51
damr <damr18@gmail.com>
2021-04-08 08:31:27 -0500

    only show delete if user is admin

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import Like from "./like";
-import {Link} from 'react-router-dom'
-import Table from "./table"
+import { Link } from "react-router-dom";
+import Table from "./table";
+import { getCurrentUser } from "../services/authService";
 
 class MoviesTable extends Component {
   columns = [
-    { path: "title", label: "Title" , content:movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link> },
+    {
+      path: "title",
+      label: "Title",
+      content: (movie) => (
+        <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
+      ),
+    },
     { path: "genre.name", label: "Genre" },
     { path: "numberInStock", label: "Stock" },
     { path: "dailyRentalRate", label: "Rate" },
@@ -15,24 +22,38 @@ class MoviesTable extends Component {
         <Like liked={movie.like} toggleLike={() => this.props.onLike(movie)} />
       ),
     },
-    {
-      key: "delete",
-      content: (movie) => (
-        <button
-          onClick={() => this.props.onDelete(movie)}
-          className="btn btn-danger"
-        >
-          Delete
-        </button>
-      ),
-    },
   ];
+
+  deleteColumn = {
+    key: "delete",
+    content: (movie) => (
+      <button
+        onClick={() => this.props.onDelete(movie)}
+        className="btn btn-danger"
+      >
+        Delete
+      </button>
+    ),
+  };
+
+  constructor() {
+    super();
+    const user = getCurrentUser();
+    if (user && user.isAdmin) {
+      this.columns.push(this.deleteColumn);
+    }
+  }
 
   render() {
     const { movies, sortColumn, onSort } = this.props;
 
     return (
-     <Table columns={this.columns} data={movies} sortColumn={sortColumn} onSort={onSort}  />
+      <Table
+        columns={this.columns}
+        data={movies}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      />
     );
   }
 }
